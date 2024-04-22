781986ffc4cfa5b07ec19057bfc7a1ef546c222c
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 14:02:20 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // write code here
+<script>
+        for (let m=2; m<13; m++) {
+            for (let n=1; n<13; n++) {
+                document.querySelector('#p1').innerHTML += `${m}x${n} = ${m*n} <br>`  
+            }   // for n
+            document.querySelector('#p1').innerHTML += "<hr>"
+        }   //for m
+    </script>
